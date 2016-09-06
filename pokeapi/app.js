04856868ac542dc7e121bf9a595e7ce7e0b9805d
Geraldine Monsalve <gnmonsalve@uc.cl>
2016-09-05 23:38:39 -0300

    added HATEOAS

--- pokeapi/app.js
+++ pokeapi/app.js
@@ -1,15 +1,15 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var db = require('./model/db');
-var pokemahn = require('./model/pokemahns');
-var routes = require('./routes/index');
-var pokemahns = require('./routes/pokemahns');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const db = require('./model/db');
+const pokemahn = require('./model/pokemahns');
+const routes = require('./routes/index');
+const pokemahns = require('./routes/pokemahns');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,18 +17,19 @@ app.set('view engine', 'pug');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/material-design-lite', express.static(__dirname + '/node_modules/material-design-lite/'));
 
+app.use('/material-design-lite', express.static(__dirname + '/node_modules/material-design-lite/'));
 app.use('/', routes);
 app.use('/pokemahns', pokemahns);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
--- pokeapi/routes/pokemahns.js
+++ pokeapi/routes/pokemahns.js
@@ -1,16 +1,15 @@
-var express = require('express');
-var router = express.Router();
-var mongoose = require('mongoose');
-var paginate = require('paginate')({
-    mongoose: mongoose
-});
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+const express = require('express');
+const router = express.Router();
+
+const mongoose = require('mongoose');
+const paginate = require('paginate')({ mongoose: mongoose });
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 router.use(bodyParser.urlencoded({ extended: true }))
-router.use(methodOverride(function(req, res){
+router.use(methodOverride((req, res) => {
   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    var method = req.body._method
+    const method = req.body._method
     delete req.body._method
     return method
   }
@@ -20,112 +19,161 @@ router.route('/')
   .get(function(req, res, next) {
     res.format({
       html: function(){
-        mongoose.model('Pokemahn').find().sort({id: 'asc'}).paginate({ page: req.query.page, perPage: 12 }, (err, pokemahns) => {
-          if (err) {
-            return console.error(err);
-          } else {
-            res.render('pokemahns/index', {
-              title: 'First Generation',
-              "pokemahns" : pokemahns
-            });
-          }
-        });
+        mongoose
+          .model('Pokemahn')
+          .find()
+          .sort({id: 'asc'})
+          .paginate(
+            { page: req.query.page, perPage: 16 },
+            (err, pokemahns) => {
+              if (err) return console.error(err);
+              else res.render(
+                'pokemahns/index',
+                { title: 'First Generation', "pokemahns" : pokemahns });
+            }
+          );
       },
       json: function(){
-        mongoose.model('Pokemahn').find().sort({id: 'asc'}).exec(function (err, pokemahns) {
-          if (err) {
-            return console.error(err);
-          } else {
+        mongoose
+          .model('Pokemahn')
+          .find()
+          .sort({id: 'asc'})
+          .exec((err, pokemahns) => {
+            if (err) return console.error(err);
+            pokemahns.links = {
+              self: {
+                href: `${req.protocol}://${req.headers.host}/pokemahns`,
+              },
+              home: {
+                href: `${req.protocol}://${req.headers.host}`,
+              },
+            };
             res.json(pokemahns);
-          }
-        });
+          });
       }
     });
   })
   .post(function(req, res) {
-    var name = req.body.name;
-    var type = req.body.type;
-    var hp = req.body.hp;
-    var attack = req.body.attack;
-    var defense = req.body.defense;
-    var speed = req.body.speed;
-    var special_attack = req.body.special_attack;
-    var special_defense = req.body.special_defense;
-    var evolve_level = req.body.evolve_level;
-    var evolve_to = req.body.evolve_to;
-    var moves = req.body.moves;
+    const name = req.body.name;
+    const type = req.body.type;
+    const hp = req.body.hp;
+    const attack = req.body.attack;
+    const defense = req.body.defense;
+    const speed = req.body.speed;
+    const special_attack = req.body.special_attack;
+    const special_defense = req.body.special_defense;
+    const evolve_level = req.body.evolve_level;
+    const evolve_to = req.body.evolve_to;
+    const moves = req.body.moves;
 
     mongoose.model('Pokemahn').create({
-      name: name,
-      type: type,
-      hp: hp,
-      attack: attack,
-      defense: defense,
-      speed: speed,
-      special_attack: special_attack,
-      special_defense: special_defense,
-      evolve_level: evolve_level,
-      evolve_to: evolve_to,
-      moves: moves
-    }, function (err, pokemahn) {
+      name,
+      type,
+      hp,
+      attack,
+      defense,
+      speed,
+      special_attack,
+      special_defense,
+      evolve_level,
+      evolve_to,
+      moves,
+    }, (err, pokemahn) => {
       if (err) {
         res.send("There was a problem adding your pokemahn to the pokedex.");
-      } else {
+      }
+      else {
         console.log('POST creating new pokemahn: ' + pokemahn);
         res.format({
-          html: function(){
+          html: () => {
             res.location("pokemahns");
             res.redirect("/pokemahns");
           },
-          json: function(){
-            res.json(pokemahn);
-          }
+          json: () => {
+            pokemahn.links = {
+              self: {
+                 href: `${req.protocol}://${req.headers.host}/pokemahns/${pokemahn.id}`,
+               },
+               home: {
+                 href: `${req.protocol}://${req.headers.host}`,
+               },
+               pokemahns: {
+                 href: `${req.protocol}://${req.headers.host}/pokemahns`,
+               },
+            };
+            res.json(pokemahn); }
         });
       }
     })
   });
 
-router.param('id', function(req, res, next, id) {
-  mongoose.model('Pokemahn').findOne({ "id": req.id }, function (err, pokemahn) {
-    if (err) {
-      console.log('Pokemahn #' + id + ' was not found');
-      res.status(404)
-      var err = new Error('Not Found');
-      err.status = 404;
-      res.format({
-        html: function(){
-          next(err);
-         },
-        json: function(){
-          res.json({message : err.status  + ' ' + err});
+router.param('id', (req, res, next, id) => {
+  mongoose
+    .model('Pokemahn')
+    .findOne(
+      { "id": req.id },
+      (err, pokemahn) => {
+        if (err) {
+          console.log('Pokemahn #' + id + ' was not found');
+          res.status(404)
+
+          const err = new Error('Not Found');
+          err.status = 404;
+
+          res.format({
+            html: () => { next(err); },
+            json: () => { res.json(
+              {
+                message : err.status  + ' ' + err,
+                // home: {
+                //   ref: `${req.protocol}://${req.headers.host}`,
+                // },
+                // pokemahns: {
+                //   href: `${req.protocol}://${req.headers.host}/pokemahns`,
+                // },
+              });
+            }
+          });
+        } else {
+          req.id = id;
+          next();
         }
-      });
-    } else {
-      req.id = id;
-      next();
-    }
-  });
+      }
+    );
 });
 
 router.route('/:id')
-  .get(function(req, res) {
-    mongoose.model('Pokemahn').findOne({ "id": req.id }, function (err, pokemahn) {
-      if (err) {
-        console.log('GET Error: There was a problem retrieving: ' + err);
-      } else {
-        console.log('GET Retrieving ID: ' + pokemahn.id);
-        res.format({
-          html: function(){
-              res.render('pokemahns/show', {
-                "pokemahn" : pokemahn,
-              });
-          },
-          json: function(){
-              res.json(pokemahn);
+  .get((req, res) => {
+    mongoose
+      .model('Pokemahn')
+      .findOne(
+        { "id": req.id },
+        (err, pokemahn) => {
+          if (err) {
+            console.log('GET Error: There was a problem retrieving: ' + err);
+          } else {
+            console.log('GET Retrieving ID: ' + pokemahn.id);
+            res.format({
+              html: () => {
+                res.render('pokemahns/show', { "pokemahn" : pokemahn });
+              },
+              json: () => {
+                pokemahn.links = {
+                  self: {
+                     href: `${req.protocol}://${req.headers.host}/pokemahns/${pokemahn.id}`,
+                   },
+                   home: {
+                     href: `${req.protocol}://${req.headers.host}`,
+                   },
+                   pokemahns: {
+                     href: `${req.protocol}://${req.headers.host}/pokemahns`,
+                   },
+                };
+                res.json(pokemahn);
+              }
+            });
           }
         });
-      }
-    });
   });
 
 module.exports = router;
